bd99740a5d5e0163ec51151e5d1b9a7eeeeeb376
mbattaloglulab <mustafabattaloglu@posta.mu.edu.tr>
2021-10-14 15:17:11 +0300

    Initial

---
+++ app/src/main/java/com/example/week3lab/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.week3lab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<Post> posts = new ArrayList<>();
+    ListView listView;
+
+    static final int POST_REQUEST = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        PostAdapter adapter = new PostAdapter(this, posts);
+        listView.setAdapter(adapter);
+
+        Button btnPost =  (Button)findViewById(R.id.btnPost);
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                startActivityForResult(intent, POST_REQUEST );
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == POST_REQUEST && resultCode == Activity.RESULT_OK) {
+            Post post = new Post();
+            post.setMessage(data.getCharSequenceExtra("msg").toString());
+            post.setImage((Bitmap) data.getParcelableExtra("bitmap"));
+            posts.add(post);
+            ((PostAdapter) listView.getAdapter()).notifyDataSetChanged();
+        }
+    }
+
+}
